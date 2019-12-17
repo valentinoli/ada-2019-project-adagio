ffba42501a327c2db3d3044affeef186ec5fcfbf
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2019-12-17 16:30:02 +0100

    Countries not hardcode

--- src/scripts/helpers.py
+++ src/scripts/helpers.py
@@ -19,14 +19,6 @@ nearby_countries = [
 ]
 
 
-def compute_nearby_imports_ratio(imports):
-    """Compute the ratio of imported products which come from nearby countries"""
-    imported_nearby = imports[imports.commercial_partner.isin(nearby_countries)]
-    total_nearby = imported_nearby.quantity_kg.sum()
-    total_overall = imports.iloc[0, 1]
-    return total_nearby / total_overall
-
-
 def load_countries_continents():
     """Load countries and the continents they belong to"""
     return pd.read_excel(
